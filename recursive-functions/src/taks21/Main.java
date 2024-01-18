a51d273c8cb89447faceba5a620cdeb46c48d9e9
lubobul <lyubomir2stoychev@gmail.com>
2024-01-18 15:26:09 +0200

    - improvements over reading inputs for the function

--- recursive-functions/src/taks21/Main.java
+++ recursive-functions/src/taks21/Main.java
@@ -6,29 +6,40 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n;
-        double x;
+        String keepTestingAnswer = "";
+        do {
+            int n;
+            double x;
 
-        do{
-            System.out.print("Enter integer between 0 and 10^6: ");
-            n = scanner.nextInt();
-        }while(n < 0 || n > Math.pow(10, 6));
+            do {
+                System.out.print("Enter integer between 0 and 10^6: ");
+                n = scanner.nextInt();
+            } while (n < 0 || n > Math.pow(10, 6));
 
-        do{
-            System.out.print("Enter a positive real number: ");
-            x = scanner.nextDouble();
-        }while(x < 0);
+            do {
+                System.out.print("Enter a positive real number: ");
+                x = scanner.nextDouble();
+            } while (x < 0);
 
-        System.out.println(f(n, x));
+            scanner.nextLine();
+
+            System.out.println(f(n, x));
+            System.out.print("To keep testing, type Yes, to quit press return: ");
+            keepTestingAnswer = scanner.nextLine();
+            int a = 0;
+        }while (keepTestingAnswer.equals("Yes"));
+
+        scanner.close();
+
     }
 
-    public static double f(int n, double x){
+    public static double f(int n, double x) {
 
-        if(n >= 3 && Math.abs(x) >= Math.pow(10, 3)){
-            if(n % 2 == 1){
-                return f(n-1, Math.sqrt(x)) - (5 * Math.sqrt(n));
-            }else{
-                return Math.sqrt(Math.abs(f(n-1, Math.sqrt(x)) - f(n-2, Math.sqrt(x))));
+        if (n >= 3 && Math.abs(x) >= Math.pow(10, 3)) {
+            if (n % 2 == 1) {
+                return f(n - 1, Math.sqrt(x)) - (5 * Math.sqrt(n));
+            } else {
+                return Math.sqrt(Math.abs(f(n - 1, Math.sqrt(x)) - f(n - 2, Math.sqrt(x))));
             }
         }
 
